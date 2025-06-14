6e71dcb33861c9230039a913b64cd1112293e909
chancehudson <jchancehud@gmail.com>
2025-06-14 18:36:24 -0500

    feat: playtest server script

---
+++ scripts/update_playtest.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DEV_IP=167.99.247.110
+WORKDIR=$(mktemp -d)
+cd $WORKDIR
+
+git clone http://heracles:3000/chance/game_test.git
+
+tar -czf game.tar.gz game_test
+scp game.tar.gz root@$DEV_IP:~/
+ssh root@$DEV_IP "rm -rf ~/game_test"
+ssh root@$DEV_IP "tar -xf ./game.tar.gz"
+ssh root@$DEV_IP "cd ~/game_test && /root/.cargo/bin/cargo build --features=server --no-default-features"
+#ssh root@$DEV_IP "echo \"#!/bin/bash
+#cd game_test 
+#/root/.cargo/bin/cargo build --features=server --no-default-features 
+#\" > ~/build.sh"
+#ssh root@$DEV_IP "sh ~/build.sh"
+#ssh root@$DEV_IP "rm ~/build.sh"
+
