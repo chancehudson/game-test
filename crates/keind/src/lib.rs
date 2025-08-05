7a7dfc751dfb5f2c1fb5f9a410d1fc73a7efd844
chance <jchancehud@gmail.com>
2025-08-05 03:58:26 -0500

    fix: conditionally move between Arc and Rc

--- crates/keind/src/lib.rs
+++ crates/keind/src/lib.rs
@@ -10,10 +10,10 @@ mod system;
 
 /// In a zkvm we are truly single threaded, and have no use for atomics.
 /// We try to remove all atomics and thread support to improve performance.
-//#[cfg(feature = "zk")]
+#[cfg(feature = "zk")]
 pub use std::rc::Rc as RefPointer;
-//#[cfg(not(feature = "zk"))]
-//pub use std::sync::Arc as RefPointer;
+#[cfg(not(feature = "zk"))]
+pub use std::sync::Arc as RefPointer;
 
 use serde::Deserialize;
 use serde::Serialize;
@@ -33,8 +33,6 @@ pub trait GameLogic: Clone + Serialize + for<'de> Deserialize<'de> + 'static {
         + KPoly
         + Debug
         + Clone
-        // + Send //
-        // + Sync
         + Serialize
         + for<'de> Deserialize<'de>; // Enum wrapping all possible entities
     type System: KPoly + Debug + Clone + Serialize + for<'de> Deserialize<'de>; // Enum wrapping all possible systems
