7c1e7d5e1a19ccf551d64932a701f83f355e8b73
chance <jchancehud@gmail.com>
2025-08-04 23:56:27 -0500

    feat: prove 5 steps of game engine

--- packages/keind_zk/src/bin/engine.rs
+++ packages/keind_zk/src/bin/engine.rs
@@ -9,33 +9,39 @@ pub fn main() {
 
     let mut engine = GameEngine::<KeindGameLogic>::default();
 
-    // let platform = PlatformEntity::new(
-    //     engine.generate_id(),
-    //     IVec2::new(200, 200),
-    //     IVec2::new(200, 25),
-    // );
-    // let mut mob_spawner = MobSpawnEntity::new(
-    //     engine.generate_id(),
-    //     platform.position + IVec2::new(0, platform.size.y + 20),
-    //     IVec2::new(200, 1),
-    // );
-    // mob_spawner.spawn_data.max_count = 30;
-    // mob_spawner.spawn_data.mob_type = 1;
-    // engine.register_event(
-    //     None,
-    //     EngineEvent::SpawnEntity {
-    //         entity: EngineEntity::Platform(platform),
-    //         universal: true,
-    //     },
-    // );
-    // engine.register_event(
-    //     None,
-    //     EngineEvent::SpawnEntity {
-    //         entity: EngineEntity::MobSpawner(mob_spawner),
-    //         universal: true,
-    //     },
-    // );
-    // engine.step_to(&1000);
-    //
-    // sp1_zkvm::io::commit(&engine.generate_id());
+    let platform = PlatformEntity::new(
+        BaseEntityState {
+            id: engine.generate_id(),
+            position: IVec2::new(200, 200),
+            size: IVec2::new(200, 25),
+            ..Default::default()
+        },
+        vec![],
+    );
+    let mut mob_spawner = MobSpawnEntity::new(
+        BaseEntityState {
+            id: engine.generate_id(),
+            position: platform.position() + IVec2::new(0, platform.size().y + 20),
+            size: IVec2::new(200, 20),
+            ..Default::default()
+        },
+        vec![],
+    );
+    mob_spawner.spawn_data.max_count = 30;
+    mob_spawner.spawn_data.mob_type = 1;
+    engine.register_event(
+        None,
+        EngineEvent::SpawnEntity {
+            entity: RefPointer::new(platform.into()),
+            is_non_determinism: true,
+        },
+    );
+    engine.register_event(
+        None,
+        EngineEvent::SpawnEntity {
+            entity: RefPointer::new(mob_spawner.into()),
+            is_non_determinism: true,
+        },
+    );
+    engine.step_to(&5);
 }
